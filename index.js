57b646825f12da8fd9fb4db05fbca6aaa63bb162
anik1606@gmail.com <anik1606@gmail.com>
2018-09-13 19:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    console.log("I can't hear you!");
   }
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
-    return "YES INDEED!"
+    console.log("YES INDEED!");
   }
 }
 
